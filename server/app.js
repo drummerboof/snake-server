310170257f1a504a6babb051a541179582d3e8b2
Chris James <drummerboof@gmail.com>
2013-03-24 04:33:02 +0000

    Tweaked test game settings

--- server/app.js
+++ server/app.js
@@ -39,9 +39,9 @@ server.listen(app.get('port'), function(){
 });
 gameServer = new GameServer(io, '/games');
 gameServer.createGame({
-    width: 25,
-    height: 25,
-    speed: 100
+    width: 50,
+    height: 50,
+    speed: 80
 }, 'test');
 
 routes.inject({
